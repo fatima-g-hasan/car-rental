ddce5e1b2e220afc0060623570be6ff4ac14c99d
thecoder <fathas235@gmail.com>
2025-02-27 22:45:00 +0200

    create car class and override

--- car-rental.py
+++ car-rental.py
@@ -9,4 +9,12 @@ class Vehicle:
     print(f"{self.brand} {self.model}, Year: {self.year}, Rental price: {self.rental_price_per_day}$ /day")
 
   def calculate_rental_cost(self, days):
-        return self.rental_price_per_day * days
+    return self.rental_price_per_day * days
+  
+class Car(Vehicle):
+  def __init__(self, brand, model, year, rental_price_per_day, seating_capacity):
+    super().__init__(brand, model, year, rental_price_per_day)
+    self.seating_capacity = seating_capacity
+
+  def display_info(self):
+    print(f"Car: {self.brand} {self.model}, Year: {self.year}, Seats: {self.seating_capacity}, Rental price: {self.get_rental_price()}$ /day")
