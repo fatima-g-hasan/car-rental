e95123d3e1e6d64207587249bbadfb360aa9af3d
thecoder <fathas235@gmail.com>
2025-02-27 22:30:18 +0200

    create display_info() and calculate_rental_cost()

--- car-rental.py
+++ car-rental.py
@@ -4,3 +4,9 @@ class Vehicle:
     self.model = model
     self.year = year
     self.rental_price_per_day = rental_price_per_day
+
+  def display_info(self):
+    print(f"{self.brand} {self.model}, Year: {self.year}, Rental price: {self.rental_price_per_day}$ /day")
+
+  def calculate_rental_cost(self, days):
+        return self.rental_price_per_day * days
