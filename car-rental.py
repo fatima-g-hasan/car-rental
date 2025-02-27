e88bbffb0fd4e2bf91ccf2edb70980729a4e4297
thecoder <fathas235@gmail.com>
2025-02-27 22:51:20 +0200

    create bike class and override

--- car-rental.py
+++ car-rental.py
@@ -18,3 +18,11 @@ class Car(Vehicle):
 
   def display_info(self):
     print(f"Car: {self.brand} {self.model}, Year: {self.year}, Seats: {self.seating_capacity}, Rental price: {self.get_rental_price()}$ /day")
+
+class Bike(Vehicle):
+  def __init__(self, brand, model, year, rental_price_per_day, engine_capacity):
+    super().__init__(brand, model, year, rental_price_per_day)
+    self.engine_capacity = engine_capacity
+
+  def display_info(self):
+    print(f"Bike: {self.brand} {self.model}, Year: {self.year}, Engine: {self.engine_capacity}cc, Rental price: {self.get_rental_price()}$ /day")
